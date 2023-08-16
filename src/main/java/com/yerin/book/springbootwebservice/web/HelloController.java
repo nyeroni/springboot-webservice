00ad5898dfe5cea8bc93d031c037cdf0637e3cc0
nyeroni <nir2y@naver.com>
2023-08-17 04:26:04 +0900

    Feat: 테스트컨트롤러를 검증할 테스트코드 작성

--- src/main/java/com/yerin/book/springbootwebservice/web/HelloController.java
+++ src/main/java/com/yerin/book/springbootwebservice/web/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @GetMapping("/helo")
+    @GetMapping("/hello")
     public String hello(){
         return "hello";
     }

ee24d54987e84ed2a13430f6b96c41c106ef8ffc
nyeroni <nir2y@naver.com>
2023-09-28 04:31:57 +0900

    Update: 오타 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,17 +3,16 @@
 REPOSITORY=/home/ec2-user/app/step2
 PROJECT_NAME=springboot-webservice
 
-echo "> Build 파일복사"
-cp $REPOSITORY/zip/*.jar %REPOSITORY/
+echo "> Build 파일 복사"
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
+echo "> 현재 구동중인 애플리케이션 pid 확인"
+CURRENT_PID=$(pgrep -fl ${PROJECT_NAME} | grep jar | awk '{print $1}')
 
-echo "> 현재 구동 중인 애플리케이션 pid 확인"
-CURRENT_PID=$(pgrep -fl springboot-webservice | grep jar | awk '{print $1}')
-
-echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
+echo "> 현재 구동중인 애플리케이션 pid: $CURRENT_PID"
 
 if [ -z "$CURRENT_PID" ]; then
-        echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+        echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다"
 else
         echo "> kill -15 $CURRENT_PID"
         kill -15 $CURRENT_PID
@@ -32,7 +31,6 @@ chmod +x $JAR_NAME
 echo "> $JAR_NAME 실행"
 
 nohup java -jar \
-        -Dspring.config.location=classpath:/application.yml,/home/ec2-user/app/application-oauth.yml,/home/ec2-user/app/application-real-db.yml,classpath:/application-real.yml \
+        -Dspring.config.location="classpath:/application.yml","/home/ec2-user/app/application-oauth.yml","/home/ec2-user/app/application-real-db.yml","classpath:/application-real.yml" \
         -Dspring.profiles.active=real \
         $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
-
